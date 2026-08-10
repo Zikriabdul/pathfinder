b3ac46ab09eb873cd9a6d37cc906680d9fc88965
Zikriabdul <keduazikri@gmail.com>
2026-08-10 08:25:11 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -513,7 +513,7 @@ async function selesaiTes() {
 `;
   setTimeout(() => {
     document.getElementById("survey1").style.display = "flex";
-  }, 1800);
+  }, 2200);
 
   hasilLengkap = {
     nama: namaPeserta,
